b98d78dc1aa85393a57e40424b2aceac000e511b
Maria Alejandra Montenegro <thinkmariale@gmail.com>
2017-09-26 00:49:38 -0700

    another test

--- js/MusicPlayer/myplaylist.js
+++ js/MusicPlayer/myplaylist.js
@@ -7,7 +7,6 @@
 var myPlaylist = [
 	 {
         mp3: 'https://dl.dropboxusercontent.com/u/12949839/Recort%C3%A9%20un%20pedazo%20del%20cielo.wav',
-        oga: 'https://db.tt/ajgJbhe2',
         title: 'Recorté un pedazo del cielo',
         artist: 'Numasbala, Mapas Perdidos',
         duration: '4:00',
@@ -15,7 +14,6 @@ var myPlaylist = [
     },
 	 {
         mp3: 'https://dl.dropboxusercontent.com/u/12949839/11%20Un%20pez%20.wav',
-        oga: 'https://db.tt/ajgJbhe2',
         title: 'Un Péz',
         artist: 'Numasbala, Mapas Perdidos',
         duration: '5:30',
@@ -23,7 +21,6 @@ var myPlaylist = [
     },
 	 {
         mp3: 'https://dl.dropboxusercontent.com/u/12949839/02%20Yuhu.wav',
-        oga: 'https://db.tt/ajgJbhe2',
         title: 'Yuhu',
         artist: 'Numasbala, Mapas Perdidos',
         duration: '3:25',
@@ -31,7 +28,6 @@ var myPlaylist = [
     },
 	 {
         mp3: 'https://dl.dropboxusercontent.com/u/12949839/08%20Calma.wav',
-        oga: 'https://db.tt/ajgJbhe2',
         title: 'Calma',
         artist: 'Numasbala, Mapas Perdidos',
         duration: '3:30',
@@ -39,67 +35,10 @@ var myPlaylist = [
     },
 	 {
         mp3: 'https://dl.dropboxusercontent.com/u/12949839/03_Nariz_de_payaso.wav',
-        oga: 'https://db.tt/ajgJbhe2',
         title: 'Nariz de Payaso',
         artist: 'Numasbala, Operacion Conexi&#243;n',
         duration: '4:15',
         cover: 'images/operacion_c.jpg'
     },
-    {
-        mp3: 'https://db.tt/ajgJbhe2',
-        oga: 'https://db.tt/ajgJbhe2',
-        title: 'Guapa',
-        artist: 'Numasbala, A Ver',
-        duration: '3:56',
-        cover: 'images/librito.jpg'
-    },
-    {
-        mp3: 'https://db.tt/qns7QRKS',
-        oga: 'https://db.tt/qns7QRKS',
-        title: 'Su&eacute;ltame',
-        artist: 'Numasbala, A Ver',
-        duration: '3:22',
-        cover: 'images/librito.jpg'
-    },
-    {
-         mp3: 'https://db.tt/sxCQTDMI',
-         omg: 'https://db.tt/sxCQTDMI',
-         title: 'Qu&eacute;date',
-         artist: 'Numasbala, A Ver',
-         duration: '4:47',
-         cover: 'images/librito.jpg'
-     },
-     {
-        mp3: 'https://db.tt/tp6FPwY2',
-        oga: 'https://db.tt/tp6FPwY2',
-         title: 'El &#193;guila Maligna',
-         artist: 'Numasbala, A Ver',
-         duration: '4:07',
-         cover: 'images/librito.jpg'
-     },
-      {
-        mp3: 'https://db.tt/mDazue21',
-        oga: 'https://db.tt/mDazue21',
-         title: 'Mujeres',
-         artist: 'Numasbala, A Ver',
-         duration: '4:11',
-         cover: 'images/librito.jpg'
-     },
-      {
-        mp3: 'https://db.tt/etf3A8Mw',
-        oga: 'https://db.tt/etf3A8Mw',       
-         title: 'Oye Coraz&#243;n',
-         artist: 'Numasbala, A Ver',
-         duration: '4:28',
-         cover: 'images/librito.jpg'
-     },
-
-     {
-        mp3: 'https://db.tt/k1edftJ4',
-        oga: 'https://db.tt/k1edftJ4',
-        title: 'Chica Robot',
-        artist: 'Numasbala, A Ver',
-        duration: '3:11',
-       cover: 'images/librito.jpg'
-    },
+   
 ];
